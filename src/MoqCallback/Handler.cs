52f0fe38d7ec61f6d0471b297caf68d01a2f1133
Colin Pear <cpear@hotmail.com>
2017-02-25 12:15:05 -0600

    Main files

---
+++ src/MoqCallback/Handler.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+
+namespace MoqCallback
+{
+    public class Handler
+    {
+        private readonly IDosomething _iDo;
+
+        public Handler(IDosomething iDo)
+        {
+            _iDo = iDo;
+        }
+
+        public async Task Handle()
+        {
+            var testValueToReturn = 700;
+
+            await _iDo.DoIt(testValueToReturn);
+        }
+    }
+
+    public interface IDosomething
+    {
+        Task DoIt(int testValue);
+    }
+}
---
+++ src/MoqCallback/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Moq;
+
+namespace MoqCallback
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int result;
+
+            var mockDoIt = new Mock<IDosomething>();
+            mockDoIt.Setup(x => x.DoIt(It.IsAny<int>())).Callback<int>(r => result = r);
+
+            var handler = new Handler(mockDoIt.Object);
+            handler.Handle().Wait();
+
+        }
+    }
+}
